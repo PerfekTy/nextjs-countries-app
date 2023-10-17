23a1c556101f2f1332885951ab7206d8f7e45b05
PerfekTyy <sebastianfelenczak@gmail.com>
2023-10-18 00:10:25 +0200

    fixed deploy issues

--- app/api/[countryName]/route.ts
+++ app/api/[countryName]/route.ts
@@ -19,8 +19,8 @@ export async function GET(req: NextRequest) {
     );
 
     return new Response(JSON.stringify(filteredData), { status: 200 });
-  } catch (err) {
-    return new Response("Something went wrong", { status: 500 });
+  } catch (err: any) {
+    return new Response(err.message, { status: 500 });
   }
 }
 
